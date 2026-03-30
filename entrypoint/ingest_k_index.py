15998738552634e0659ecda3e6efafb28ec53028
Keith <khowen@student.unimelb.edu.au>
2026-03-30 17:15:56 +1100

    Renamed `ingest_k_index_with_logging.py` to `ingest_k_index.py` in entrypoint since logging logic already finalized

--- entrypoint/ingest_k_index.py
+++ entrypoint/ingest_k_index.py
@@ -1,7 +1,7 @@
 """
 How to run `entrypoint/ingest_k_index.py in the terminal:
 
-`python -m entrypoint.ingest_k_index --config_path "config/local.yaml" --location "Australian regionnnn" --start "2025-01-01 00:00:00" --end "2025-02-02 00:00:00"`
+`python -m entrypoint.ingest_k_index_with_logging --config_path "config/local.yaml" --location "Australian region" --start "2018-01-01 00:00:00" --end "2019-02-02 00:00:00"`
 
 Why use `-m`?
 > We agreed that importing custom modules must start from the root (e.g. src.io.load_config, src.ingest.space_weather_k_index)
@@ -37,20 +37,14 @@ import argparse
 
 from src.io.load_config import load_config
 from src.ingest.space_weather_k_index import ingest_k_index_run
-
+from src.utils.logging import run_entrypoint_with_logging
 
 
 import logging
 logger = logging.getLogger(__name__)
 
- 
-def main():
 
-    logging.basicConfig(
-        level=logging.INFO, # Set the minimum level to log (INFO, DEBUG, WARNING, ERROR, CRITICAL)
-        format='[%(asctime)s - %(levelname)s] %(message)s', # Define the format
-        handlers=[logging.StreamHandler()] # Ensure it prints to the console
-    )
+def parse_args() -> argparse.Namespace:
 
     # initialize the ArgumentParser
     parser = argparse.ArgumentParser(description="CLI for K-index Ingestion.")
@@ -70,26 +64,52 @@ def main():
     # 5. add directory to save ingested K-index to
     parser.add_argument('--raw_base_dir', help="Optional relative path to save the K-index to, from the project root. If empty, read from config.")
 
+    return parser.parse_args()
+
+
+ 
+def main():
+
+
     # Parse all arguments as strings into NameSpace(config_path=..., location=..., ...)
-    args = parser.parse_args()
+    args = parse_args()
 
-    # load the YAML config
-    config = load_config(args.config_path)
-    sw_config = config['space_weather']
 
-    # since start and end dates CAN be unspecifed (None type),
-    # we need to parse it correctly as actual Nonetype (and not "None")
-    start_args = args.start if args.start != "None" else None
-    end_args = args.end if args.end != "None" else None
-    
-    # start the ingestion
-    ingest_k_index_run(
-        sw_config,
-        location=args.location,
-        start=start_args,
-        end=end_args,
-        raw_base_dir=args.raw_base_dir
+    def _main_logic(logger: logging.Logger) -> None:
+
+        """
+        Main logic for K-index ingestion
+        """
+        # load the YAML config
+        config = load_config(args.config_path)
+        sw_config = config['space_weather']
+
+        # since start and end dates CAN be unspecifed (None type),
+        # we need to parse it correctly as actual Nonetype (and not "None")
+        start_args = args.start if args.start != "None" else None
+        end_args = args.end if args.end != "None" else None
+
+        logger.info(f"Starting K-index ingestion for location={args.location}.")
+        
+        # start the ingestion
+        ingest_k_index_run(
+            sw_config,
+            location=args.location,
+            start=start_args,
+            end=end_args,
+            raw_base_dir=args.raw_base_dir
+        )
+        pass
+
+
+    # run the main logic with standardized logging setup and final log file renaming
+    run_entrypoint_with_logging(
+        entrypoint_name="ingest_k_index",
+        main_logic=_main_logic,
+        log_dir="logs",
     )
+    
+
 
 if __name__ == "__main__":
     main()
